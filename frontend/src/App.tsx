03a8d84695927c1a03d3d36c60f53e211f89e728
Lukasz <djpluki@gmail.com>
2020-11-20 06:14:25 +0100

    add toasts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import 'react-toastify/dist/ReactToastify.css';
+
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ApolloProvider } from '@apollo/client';
 import { Normalize } from 'styled-normalize';
 
+import { ToastWrapper } from './components/Elements/ToastElement/ToastWrapper';
 import { client } from './graphql';
 import { Routing } from './Routing/Routing';
 import { store } from './store';
@@ -14,6 +17,7 @@ function App() {
       <ApolloProvider client={client}>
         <Normalize />
         <GlobalStyles />
+        <ToastWrapper />
 
         <Routing />
       </ApolloProvider>
---
+++ frontend/src/components/Elements/ToastElement/ToastWrapper.tsx
@@ -0,0 +1,31 @@
+import 'react-toastify/dist/ReactToastify.css';
+
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { rem } from 'polished';
+import styled from 'styled-components';
+
+const Wrapper = styled(ToastContainer).attrs({
+  // custom props
+})`
+  .Toastify__toast-container {
+  }
+  .Toastify__toast {
+    min-height: auto;
+    padding: ${rem(10)};
+  }
+  .Toastify__toast--error {
+  }
+  .Toastify__toast--warning {
+  }
+  .Toastify__toast--success {
+  }
+  .Toastify__toast-body {
+  }
+  .Toastify__progress-bar {
+  }
+`;
+
+export const ToastWrapper = () => {
+  return <Wrapper autoClose={5000} closeOnClick hideProgressBar />;
+};
